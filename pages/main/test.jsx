ba6be6294b409d10f0f7e159682a83c49299c3b4
rakshith cyril <rakshithcyril@yahoo.com>
2022-11-01 03:08:37 +0530

    test

---
+++ pages/main/test.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Head from "next/head";
+
+export default class Test extends React.Component{
+    render(){
+        return(
+            <div>
+                <Head>
+                    <title>test</title>
+                </Head>
+                <main>
+                    <h1>test</h1>
+                </main>
+            </div>
+        )
+    }
+}
